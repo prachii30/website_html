ab9844ccae7642f1580ba45ac6b7347218199c63
prachi solanki <prachisolanki3001@gmail.com>
2025-08-12 19:32:40 +0530

    added index.html

---
+++ prachi/download3.php
@@ -0,0 +1,19 @@
+<?php
+// Path to the file
+$file ='10thEM.pdf';
+
+// Check if file exists
+if (file_exists($file)) {
+    header('Content-Description: File Transfer');
+    header('Content-Type: application/octet-stream');
+    header('Content-Disposition: attachment; filename="' . basename($file) . '"');
+    header('Expires: 0');
+    header('Cache-Control: must-revalidate');
+    header('Pragma: public');
+    header('Content-Length: ' . filesize($file));
+    readfile($file);
+    exit;
+} else {
+    echo "<h2>File not found.</h2>";
+}
+?>
